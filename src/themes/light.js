5bbc28940908f52482998193553fa3faed0a2e25
yvesgurcan <gurcan.yves+github@gmail.com>
2020-05-02 22:55:28 -0700

    Adjust light theme background color

--- src/themes/light.js
+++ src/themes/light.js
@@ -2,7 +2,7 @@ export default {
     name: 'Light',
     evenSquareBackground: 'rgb(210, 210, 210)',
     oddSquareBackground: 'rgb(150, 150, 150)',
-    background1: 'rgb(240, 240, 240)',
+    background1: 'rgb(200, 200, 200)',
     background2: 'rgb(118, 118, 118)',
     color1: 'rgb(30, 30, 30)',
     color2: 'rgb(250, 250, 250)',
